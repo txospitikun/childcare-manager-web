c05e6b421379f6782376f2fe724fbe0a7b265360
Mareantz <marianciotir@gmail.com>
2024-04-11 00:02:43 +0300

    register modal

--- frontend/modals/register_modal/register.js
+++ frontend/modals/register_modal/register.js
@@ -1,33 +1,25 @@
-class RegisterModal extends HTMLElement {
+class LoginModal extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /*html*/ `
-        <div class="register-container">
+        <div class="login-container">
             <div class="user-input-container">
-                <p>Register with an account to Childbook</p>
+                <p>Connect with an account to Childbook</p>
                 <div class="login-input-container">
-                    <label for="register-username">username</label>
-                    <input type="text" id="register-username">
+                    <label for="login-username">email or username</label>
+                    <input type="text" id="login-username">
                 </div>
                 <div class="login-input-container">
-                    <label for="register-password">password</label>
-                    <input type="password" id="register-password">
-                </div>
-                <div class="login-input-container">
-                    <label for="confirm-register-password">confirm password</label>
-                    <input type="password" id="confirm-register-password">
-                </div>
-                <div class="login-input-container">
-                    <label for="phone-number">phone number</label>
-                    <input type="text" id="phone-number">
+                    <label for="login-password">password</label>
+                    <input type="password" id="login-password">
                 </div>
                 <div class="forgot-password-text">
-                    <a href=".123">already registered?</a>
+                    <a class="modal-help-text" href=".">forgot your password?</a>
                 </div>
+                <button id="confirm-login">login</button>
             </div>
-            <button id="confirm-register">register</button>
         </div>
         `
     }
 }
 
-customElements.define('register-modal', RegisterModal);
+customElements.define('login-modal', LoginModal);
