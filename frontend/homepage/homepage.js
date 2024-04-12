3016634a2a6774ba1937adcec9b753802046d6ca
Mareantz <marianciotir@gmail.com>
2024-04-12 22:38:12 +0300

    homepage for desktop completed

--- frontend/homepage/homepage.js
+++ frontend/homepage/homepage.js
@@ -1,4 +1,14 @@
-function log()
-{
-    console.log("Hello World!");
-}
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const navbarHeight = 150;
+        const section = document.querySelector(this.getAttribute('href'));
+        const sectionPosition = section.getBoundingClientRect().top;
+
+        window.scrollBy({
+            top: sectionPosition - navbarHeight,
+            behavior: 'smooth'
+        });
+    });
+});
