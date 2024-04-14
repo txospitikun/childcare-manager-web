f03b7caf9fb66a040b48238e859d4cbc435ad99f
Mareantz <marianciotir@gmail.com>
2024-04-14 16:35:25 +0300

    added documentation

--- frontend/homepage/homepage.js
+++ frontend/homepage/homepage.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.navbar a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        const navbarHeight = 150;
+        const navbarHeight = 120;
         const section = document.querySelector(this.getAttribute('href'));
         const sectionPosition = section.getBoundingClientRect().top;
 
