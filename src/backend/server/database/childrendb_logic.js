b03ebafcf8c4af1bbd2146b91fd83ed222fe321a
chinezprost <playschan@gmail.com>
2024-06-22 20:19:41 +0300

    worked on healthcare system

--- src/backend/server/database/childrendb_logic.js
+++ src/backend/server/database/childrendb_logic.js
@@ -7,7 +7,7 @@ async function insertChildren(ID, childrenform)
     {
         const connection = await pool.getConnection();
         const query = 'INSERT INTO Childrens (FirstName, LastName, Gender, DateOfBirth, PictureRef, UserID) VALUES (?, ?, ?, ?, ?, ?)';
-        const [result] = await connection.query(query, [childrenform.FirstName, childrenform.LastName, childrenform.Gender, childrenform.DateOfBirth, childrenform.PictureRef, ID]);
+        const [result] = await connection.query(query, [childrenform.FirstName, childrenform.LastName, childrenform.Gender, childrenform.DateOfBirth, childrenform.PictureRef.path, ID]);
         connection.release();
         return result;
     }
