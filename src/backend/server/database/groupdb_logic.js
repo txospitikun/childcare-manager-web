355b3125167acb517cbd934a235c1564ddf84e4f
chinezprost <playschan@gmail.com>
2024-06-23 21:31:10 +0300

    worked a lot on group logic

--- src/backend/server/database/groupdb_logic.js
+++ src/backend/server/database/groupdb_logic.js
@@ -53,8 +53,8 @@ async function getUserGroups(ID)
             'FROM ChildrenGroups cg\n' +
             'LEFT JOIN GroupEntries ge ON cg.ID = ge.GroupID\n' +
             'LEFT JOIN Childrens c ON ge.ChildrenID = c.ID\n' +
-            'WHERE cg.UserID = ?';
-        const [result] = await connection.query(query, [ID]);
+            'WHERE c.UserID = ? OR cg.UserID = ?;';
+        const [result] = await connection.query(query, [ID, ID]);
         connection.release();
         return result;
     } catch (error) {
@@ -79,52 +79,117 @@ async function insertChildrenGroup(data)
 
 
 }
-async function deleteChildrenGroup(ID)
-{
-
+async function deleteChildrenGroup(userId, groupId, childrenId) {
+    try {
+        const connection = await pool.getConnection();
+        const query = 'DELETE FROM GroupEntries\n' +
+            'WHERE ChildrenID = ?\n' +
+            'AND GroupID = (\n' +
+            '    SELECT GroupID FROM (\n' +
+            '        SELECT ge.GroupID\n' +
+            '        FROM GroupEntries ge\n' +
+            '        JOIN ChildrenGroups cg ON ge.GroupID = cg.ID\n' +
+            '        WHERE ge.GroupID = ? AND cg.UserID = ?\n' +
+            '    ) AS subquery\n' +
+            ');';
+        const [result] = await connection.query(query, [childrenId, groupId, userId]);
+        connection.release();
+        return result;
+    } catch (error) {
+        console.error('Error deleting children group:', error);
+        throw error;
+    }
 }
 
-async function insertGroupRelation(ID, data)
+async function getGroupChildrenInfo(userId, groupId)
 {
-
+    try {
+        const connection = await pool.getConnection();
+        const query = 'SELECT c.ID, c.FirstName, c.LastName, c.PictureRef\n' +
+            'FROM Childrens c\n' +
+            'JOIN GroupEntries ge ON c.ID = ge.ChildrenID\n' +
+            'WHERE ge.GroupID = ?';
+        const [result] = await connection.query(query, [groupId]);
+        return result;
+    } catch (error)
+    {
+        console.error('Error getting group children info:', error);
+        throw error;
+    }
 }
 
-async function editGroupRelation(ID, data)
+async function getGroupChildrenRelations(userId, childrenId, groupId)
 {
-
+    try {
+        const connection = await pool.getConnection();
+        const query = 'SELECT *\n' +
+            'FROM GroupRelations\n' +
+            'WHERE ChildrenRelationOne = ? AND GroupID = ?';
+        const [result] = await connection.query(query, [childrenId, groupId]);
+        connection.release();
+        return result;
+    } catch (error) {
+        console.error('Error getting group children relations:', error);
+        throw error;
+    }
 }
 
-async function deleteGroupRelation(ID)
-{
+    async function insertGroupRelation(data)
+    {
+        try {
+            const connection = await pool.getConnection();
+            const query = 'INSERT INTO GroupRelations (GroupID, ChildrenRelationOne, ChildrenRelationTwo, TypeOfRelation) VALUES (?, ?, ?, ?)';
+            const [result] = await connection.query(query, [data.GroupID, data.ChildrenRelationOne, data.ChildrenRelationTwo, data.TypeOfRelation]);
+            connection.release();
+            return result;
+        } catch (error) {
+            console.error('Error inserting group relation:', error);
+            throw error;
+        }
+    }
+    async function editGroupRelation(ID, data) {
 
-}
+    }
 
-async function insertGroupChat(ID, data)
-{
+    async function deleteGroupRelation(ID)
+    {
+        try {
+            const connection = await pool.getConnection();
+            const query = 'DELETE FROM GroupRelations WHERE ID = ?';
+            const [result] = await connection.query(query, [ID]);
+            connection.release();
+            return result;
+        } catch (error) {
+            console.error('Error deleting group relation:', error);
+            throw error;
+        }
+    }
 
-}
+    async function insertGroupChat(ID, data) {
 
-async function deleteGroupChat(ID)
-{
+    }
 
-}
+    async function deleteGroupChat(ID) {
 
-async function getGroupChat(ID)
-{
+    }
 
-}
+    async function getGroupChat(ID) {
 
-module.exports = {
-    insertGroup,
-    editGroup,
-    deleteGroup,
-    getUserGroups,
-    insertChildrenGroup,
-    deleteChildrenGroup,
-    insertGroupRelation,
-    editGroupRelation,
-    deleteGroupRelation,
-    insertGroupChat,
-    deleteGroupChat,
-    getGroupChat
-};
+    }
+
+    module.exports = {
+        getGroupChildrenInfo,
+        insertGroup,
+        editGroup,
+        deleteGroup,
+        getUserGroups,
+        insertChildrenGroup,
+        deleteChildrenGroup,
+        insertGroupRelation,
+        editGroupRelation,
+        deleteGroupRelation,
+        insertGroupChat,
+        deleteGroupChat,
+        getGroupChildrenRelations,
+        getGroupChat
+    };
